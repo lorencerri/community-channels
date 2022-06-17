18e5c064694895d3b3b7a847972d30785f3f2042
Loren Cerri <lorencerri@gmail.com>
2022-06-17 01:11:44 -0400

    Update gui.ts

--- src/commands/Core/gui.ts
+++ src/commands/Core/gui.ts
@@ -1,4 +1,5 @@
 import { ApplyOptions } from '@sapphire/decorators';
+import { container } from '@sapphire/framework';
 import {
 	Command,
 	CommandOptions,
@@ -16,16 +17,24 @@ import { randomBytes } from 'crypto';
 		guildIds: ['608178003393904650']
 	},
 })
+
+// TODO: Rewrite this file, separate bottom half into utils file.
 export class CategoriesCommand extends Command {
 	async chatInputRun(interaction: Command.ChatInputInteraction) {
+		const { db } = container;
+
 		if (!interaction.guild) throw new Error('Sorry, this command can only run in guilds.');
 		if (!interaction.channel) throw new Error('Sorry, this command can only run in channels.');
 
+		// TODO: below
 		//const messageURL = String(interaction.options.get('message')?.value);
 		let message;
 
 		message = await interaction.channel.send('> Generating...');
 		if (!message) return interaction.reply("> Unable to fetch message.");
+
+		// TODO: Get the url from the database and parse it whenever you want to udpate the GUI
+		await db.set(`gui_${interaction.guild.id}`, message.url);
 
 		const base = `./files/${interaction.guild.id}`;
 		const count = await generateList(base, [...new Array(200)].map((_, i) => `Item #${i}`));
