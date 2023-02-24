46bd1ad716987077f3661c00dc89f0e326aeaf6f
Loren Cerri <lorencerri@gmail.com>
2023-02-24 16:11:42 -0500

    Update info.ts

--- src/commands/Core/info.ts
+++ src/commands/Core/info.ts
@@ -22,7 +22,7 @@ export class InfoCommand extends Command {
 
 		const embed = new MessageEmbed()
 			.setColor(0x8087f6)
-			.setDescription(`Community channels is an open-source Discord bot created by **@lorencerri** ([GitHub](https://github.com/lorencerri) | [Twitter](https://twitter.com/lorencerri))\n\n**Links:**\n[Commands](https://lorencerri.notion.site/Commands-9159d2d30d15429f8b35119ebced6a2c)\n[Documentation](https://lorencerri.notion.site/lorencerri/Community-Channels-75b1c971513c4b429b5071f1357e38f0)\n[Source Code](https://github.com/lorencerri/community-channels)`)
+			.setDescription(`Community channels is an open-source Discord bot created by **@lorencerri** ([GitHub](https://github.com/lorencerri), originally for the [Hypnospace](https://lorencerri.notion.site/Hypnospace-a51c36d0b3614e82a1426bb9681f0ff6) Discord server. | [Twitter](https://twitter.com/lorencerri))\n\n**Links:**\n[Commands](https://lorencerri.notion.site/Commands-9159d2d30d15429f8b35119ebced6a2c)\n[Documentation](https://lorencerri.notion.site/lorencerri/Community-Channels-75b1c971513c4b429b5071f1357e38f0)\n[Source Code](https://github.com/lorencerri/community-channels)`)
 			.setThumbnail('https://fs.plexidev.org/api/UzNMDpe.gif')
 		interaction.reply({ embeds: [embed] });
 	}
