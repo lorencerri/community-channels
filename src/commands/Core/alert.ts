0312d201c3570da8e034e35a4f7da341b421e0f6
Loren Cerri <lorencerri@gmail.com>
2023-02-24 16:01:22 -0500

    Add alert command

---
+++ src/commands/Core/alert.ts
@@ -0,0 +1,37 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import {
+	Command,
+	CommandOptions,
+	RegisterBehavior,
+} from '@sapphire/framework';
+import { MessageEmbed } from 'discord.js';
+
+@ApplyOptions<CommandOptions>({
+	name: 'alert',
+	description: 'Displays an alert about v2 changes to the bot.',
+	requiredClientPermissions: ['EMBED_LINKS', 'MANAGE_CHANNELS'],
+	chatInputCommand: {
+		register: true,
+		guildIds: ['608178003393904650']
+	},
+})
+export class AlertCommand extends Command {
+	async chatInputRun(interaction: Command.ChatInputInteraction) {
+		if (!interaction.guild) throw new Error('Sorry, this command can only run in guilds.');
+
+		const embed = new MessageEmbed()
+			.setColor(0x8087f6)
+			.setDescription(`**The search engine is back online!** You can create pages using \`/create <name>\`, assuming you have the editor role. If you have an existing page, ping <@144645791145918464> to get access to manage it. Also, the source code is available [here](https://github.com/lorencerri/community-channels) if you want to check it out. **Enjoy!**`)
+			.setThumbnail('https://fs.plexidev.org/api/UzNMDpe.gif')
+		interaction.reply({ embeds: [embed] });
+	}
+
+	registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName('info')
+				.setDescription('Displays the alert embed'),
+			{ behaviorWhenNotIdentical: RegisterBehavior.Overwrite }
+		);
+	}
+}
